6956f4a9b2e63fb2720cf53eb14e54bde036e6e0
mitchellmanware <memanware@gmail.com>
2023-10-06 14:12:24 -0400

    adhere to lintr styles

--- tests/testthat/test-time-range.R
+++ tests/testthat/test-time-range.R
@@ -1,45 +1,32 @@
 #' @author Mitchell Manware
 #' @description
 #' unit test for model output is within desired temporal range
-#' (2018 - 2022 inclusive)
-#' assumes `test_nc_output.nc` is raster data
-#' 
+#' (2018 - 2022 inclusive).
+#' assumes `test_nc_output.nc` is raster data.
 test_that("Output times are within temporal range", {
-  
   library(terra)
   library(lubridate)
-
   # 1. import model output
-  # path_results <- "./output/model_output.nc"
   path_results <- "./testdata/test_nc_output.nc"
   model_results <- terra::rast(path_results)
-  
   # 2. function
-  check_temporal_range = function(
+  check_temporal_range <- function(
     model_output = model_results,
     start_range = "2018-01-01",
     end_range = "2022-12-31"
-  ){
-    
+  ) {
     # change character inputs to dates
     start_date <- as_datetime(start_range)
     end_date <- as_datetime(end_range)
-    
     # create sequence of dates
     range <- seq(start_date, end_date, 86400)
-    
     # assign date values to variable
     dates <- as_datetime(model_output@ptr$time)
-    
     # check that dates are within range
     checked <- as.vector(dates %in% range)
     return(checked)
-
   }
-  
   iswithin <- check_temporal_range()
-  
   # expect all elements == TRUE
   expect_equal(any(iswithin), TRUE)
-  
 })
