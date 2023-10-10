895a1153c5aec7642998bdc5f41feb3aa90571a2
mitchellmanware <memanware@gmail.com>
2023-10-10 13:24:03 -0400

    create test_nc_output_1.nc

--- tests/testthat/test-time-range-point.R
+++ tests/testthat/test-time-range-point.R
@@ -4,10 +4,8 @@
 #' (2018 - 2022 inclusive).
 #' assumes `test_nc_output.nc` is point data.
 test_that("Output times are within temporal range", {
-  library(terra)
-  library(lubridate)
   # 1. import model output
-  path_results <- "./testdata/test_nc_output.nc"
+  path_results <- "./testdata/test_nc_output_1.nc"
   model_results <- terra::vect(path_results)
   # 2. function
   check_temporal_range <- function(
@@ -16,12 +14,12 @@ test_that("Output times are within temporal range", {
     end_range = "2022-12-31"
   ) {
     # change character inputs to dates
-    start_date <- as_datetime(start_range)
-    end_date <- as_datetime(end_range)
+    start_date <- lubridate::as_datetime(start_range)
+    end_date <- lubridate::as_datetime(end_range)
     # create sequence of dates
     range <- seq(start_date, end_date, 86400)
     # assign date values to variable
-    dates <- as_datetime(model_results$Date)
+    dates <- lubridate::as_datetime(model_results$Date)
     # check that dates are within range
     checked <- as.vector(dates %in% range)
     return(checked)
