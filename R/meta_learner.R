c1e3cfc279ef10b768f6adcc0e3552a3c306dd78
Kyle Messier <messierkp@ehshpclp143.hpc.niehs.nih.gov>
2023-10-11 13:15:25 -0400

    going with BART as metalearner directly - can change later if needed

--- R/meta_learner.R
+++ R/meta_learner.R
@@ -1,28 +1,47 @@
 #' meta_learner_fit
-#' Fit our meta learner. It takes predictions of other models such as
-#' kriging, GLM, machine learning models as input and fits a new model
+#' Fit a BART (Bayesian Additive Regression Tree) meta learner. It takes
+#' predictions of other models such as kriging, GLM, machine learning models as
+#' input and fits a BART Model
 #' @param base_predictor_list - a list where each
-#' @param meta_learner_obj an exported S3/S4/function that is the meta learner
-#' @param train_loc sf geospatial information on training locations
+#' @param y dependent variable
 #' @param kfolds integer, index of k-folds for cross-validation. This should be
 #' produced with regards to spatial and/or temporal considerations
 #' @return meta_fit_obj object of meta learner
 #' @export
 #' @examples NULL
-meta_learner_fit <- function(base_predictor_list, meta_learner_obj,
+meta_learner_fit <- function(base_predictor_list,
                              train_loc, kfolds) {
-
-  # Use the exported meta learner to fit the model
-  meta_fit_obj <- meta_learner_obj(base_predictor_list, train_loc, kfolds)
+  
+  # check lengths of each base predictor 
+  if (sapply(base_predictor_list, length, simplify = TRUE) |> 
+      var() != 0) {
+    error("base predictors need to be the same length")
+  }
+  
+  # convert list to data.frame
+  x.design <- as.data.frame(base_learner_list)
+  
+  # Unique k-folds (typically 5 or 10)
+  n.k <- length(unique(kfolds))
+  # Pre-allocate list of meta objects
+  meta_fit_obj <- vector(mode = "list", length = n.k)
+  for (i in 1:n.k) {
+    # get the training and test sets
+    x.tr <- x.design[i != n.k,]
+    x.te <- x.design[i == n.k,]
+    y.tr <- y[i != n.k]
+    # Fit the BART model
+    bart.fit[[i]] <- BART::mc.wbart(x.tr, y.tr, x.test = x.te)
+  }
   return(meta_fit_obj)
 }
 
 
 
-#' meta_learner_predict - take the meta_fit_obj and prediction location info
-#' to create meta_learner predictions
+#' meta_learner_predict - take the list of BART fit objects and prediction 
+#' location info to create meta_learner predictions
 #'
-#' @param meta_fit_obj S3/S4 object from meta_learner_fit
+#' @param meta_fit_obj list of BART objects from meta_learner_fit
 #' @param pred_loc sf or dataframe grid of prediction locations
 #' @return meta_pred_nc NetCDF (nc) file of the final meta learner predictions
 #' @export
@@ -31,10 +50,24 @@ meta_learner_fit <- function(base_predictor_list, meta_learner_obj,
 meta_learner_predict <- function(meta_fit_obj, pred_loc) {
 
   #Use the predict method
-  meta_pred_vec <- predict(meta_fit_obj, new = pred_loc)
+  meta_pred_vec <- BART::predict(meta_fit_obj, new = pred_loc)
   # Call the vec2nc function to create a NetCDF (nc) file from the
   # vector predictions
   meta_pred_nc <- vec2nc(meta_pred_vec)
   return(meta_pred_nc)
 
+}
+
+
+#' vec2nc takes a vector or matrix of predictions, some spatial information,
+#' and create a NetCDF
+#' @param pred_vec vector/matrix of predictions
+#' @param pred_bbox an sf bounding box defining the domain of the NetCDF
+#' 
+#' @return nc_out a NetCDF file
+#' @export
+#'
+#' @examples NULL
+vec2nc <- function(pred_vec, pred_bbox, field_names) {
+  
 }
