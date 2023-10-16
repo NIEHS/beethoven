4b6f3265fef41c2b342ea8634375b0d38ada385b
Kyle Messier <messierkp@ehshpclp143.hpc.niehs.nih.gov>
2023-10-16 12:35:34 -0400

    Styler automatic linting edits

--- R/meta_learner.R
+++ R/meta_learner.R
@@ -3,7 +3,7 @@
 #' predictions of other models such as kriging, GLM, machine learning models as
 #' input and fits a BART Model
 #' @param base_predictor_list - P x 1 list where P = p is a base predictor
-#' vector (numeric). Each predictor vector should be the same length and 
+#' vector (numeric). Each predictor vector should be the same length and
 #' named.
 #' @param y dependent variable
 #' @param kfolds integer, index of k-folds for cross-validation. This should be
@@ -13,23 +13,22 @@
 #' @examples NULL
 meta_learner_fit <- function(base_predictor_list,
                              kfolds, y) {
-  
   # check lengths of each base predictor #add a test for names
-  if (sapply(base_predictor_list, length, simplify = TRUE) |> 
-      stats::var() != 0) {
+  if (sapply(base_predictor_list, length, simplify = TRUE) |>
+    stats::var() != 0) {
     print("WARNING: base predictors need to be the same length")
   }
   # convert list to data.frame
   x.design <- as.data.frame(base_predictor_list)
-  
+
   # Unique k-folds (typically 5 or 10)
   n.k <- length(unique(kfolds))
   # Pre-allocate list of meta objects
   meta_fit_obj <- vector(mode = "list", length = n.k)
   for (i in 1:n.k) {
     # get the training and test sets
-    x.tr <- x.design[i != n.k,]
-    x.te <- x.design[i == n.k,]
+    x.tr <- x.design[i != n.k, ]
+    x.te <- x.design[i == n.k, ]
     y.tr <- y[i != n.k]
     # Fit the BART model
     meta_fit_obj[[i]] <- BART::mc.wbart(x.tr, y.tr, x.test = x.te)
@@ -39,7 +38,7 @@ meta_learner_fit <- function(base_predictor_list,
 
 
 
-#' meta_learner_predict - take the list of BART fit objects and prediction 
+#' meta_learner_predict - take the list of BART fit objects and prediction
 #' location info to create meta_learner predictions
 #'
 #' @param meta_fit_obj list of BART objects from meta_learner_fit
@@ -49,14 +48,12 @@ meta_learner_fit <- function(base_predictor_list,
 #'
 #' @examples NULL
 meta_learner_predict <- function(meta_fit_obj, pred_loc) {
-
-  #Use the predict method
+  # Use the predict method
   meta_pred_vec <- predict(meta_fit_obj, new = pred_loc)
   # Call the vec2nc function to create a NetCDF (nc) file from the
   # vector predictions
   meta_pred_nc <- vec2nc(meta_pred_vec)
   return(meta_pred_nc)
-
 }
 
 
