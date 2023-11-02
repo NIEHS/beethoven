600768c8eba6d3018384296bb3f5e30c2152c980
mitchellmanware <memanware@gmail.com>
2023-11-02 08:10:16 -0400

    tests for time range

---
+++ tests/testthat/test-time-range-raster.R
@@ -0,0 +1,30 @@
+#' @author Mitchell Manware
+#' @description
+#' unit test for model output is within desired temporal range
+#' (2018 - 2022 inclusive).
+#' assumes `test_nc_output.nc` is raster data.
+test_that("Output times are within temporal range", {
+  # 1. import model output
+  path_results <- "./testdata/test_nc_output.nc"
+  model_results <- terra::rast(path_results)
+  # 2. function
+  check_temporal_range <- function(
+    model_output = model_results,
+    start_range = "2018-01-01",
+    end_range = "2022-12-31"
+  ) {
+    # change character inputs to dates
+    start_date <- lubridate::as_datetime(start_range)
+    end_date <- lubridate::as_datetime(end_range)
+    # create sequence of dates
+    range <- seq(start_date, end_date, 86400)
+    # assign date values to variable
+    dates <- lubridate::as_datetime(model_output@ptr$time)
+    # check that dates are within range
+    checked <- as.vector(dates %in% range)
+    return(checked)
+  }
+  iswithin <- check_temporal_range()
+  # expect all elements == TRUE
+  expect_equal(any(iswithin), TRUE)
+})
