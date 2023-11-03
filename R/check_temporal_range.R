1e1756897ffb8cee5b0513d507f102721e36de3f
mitchellmanware <memanware@gmail.com>
2023-11-03 13:45:25 -0400

    create temporal range function

---
+++ R/check_temporal_range.R
@@ -0,0 +1,37 @@
+# Function to test temporal range of various data sets throughout pipeline
+# Date created: 2023-11-03
+# Mitchell Manware
+
+#' Check if input data is within user defined temporal range
+#' 
+#' @param data file path to data
+#' @param start_range beginning of temporal range
+#' @param end_range end of temporal range
+#' @param data_type Point, Polygon, or Raster data
+#' @return A logical vector reporting whether time observations in data are
+#' within defined temporal range
+#' @author Mitchell Manware
+#' @export
+check_temporal_range <- function(
+  data = NULL,
+  start_range = "2018-01-01",
+  end_range = "2022-12-31",
+  data_type = NULL
+) {
+  #### 1. create sequence of dates
+  start_date <- lubridate::as_date(start_range)
+  end_date <- lubridate::as_date(end_range)
+  range <- seq(start_date, end_date, 86400)
+  #### 2. import data and define `dates` vector
+  if (data_type %in% c("Point", "Polygon")) {
+    data <- terra::vect(data)
+    dates <- lubridate::as_date(data$Date)
+  } else if (data_type == "Raster") {
+    data <- terra::rast(data)
+    dates <- lubridate::as_date(data@ptr$time)
+  }
+  #### 3. check `dates` are within `range`
+  checked <- data.table::data.table(dates %in% range)
+  #### 4. return logical vector
+  return(checked)
+}
---
+++ tests/testthat/test-temporal-range.R
@@ -0,0 +1,15 @@
+#' @author Mitchell Manware
+#' @description
+#' unit test for model output is within desired temporal range
+#' (2018 - 2022 inclusive).
+#' assumes `test_nc_output.nc` is point data.
+test_that("Output times are within temporal range", {
+  # run check_temporal_range() function
+  iswithin <- check_temporal_range(data = "../testdata/test_nc_output.nc",
+                                   start_range = "2018-01-01",
+                                   end_range = "2022-12-31",
+                                   data_type = "Point")
+  # expect all elements == TRUE
+  expect_equal(any(iswithin), TRUE)
+})
+
