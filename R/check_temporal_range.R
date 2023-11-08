9ee4fd33365bb51b37996508f84ff9e8483c43c0
mitchellmanware <memanware@gmail.com>
2023-11-08 07:00:57 -0500

    time sequence with lubridate::as_datetime()

--- R/check_temporal_range.R
+++ R/check_temporal_range.R
@@ -19,16 +19,16 @@ check_temporal_range <- function(
   data_type = NULL
 ) {
   #### 1. create sequence of dates
-  start_date <- lubridate::as_date(start_range)
-  end_date <- lubridate::as_date(end_range)
+  start_date <- lubridate::as_datetime(start_range)
+  end_date <- lubridate::as_datetime(end_range)
   range <- seq(start_date, end_date, 86400)
   #### 2. import data and define `dates` vector
   if (data_type %in% c("Point", "Polygon")) {
     data <- terra::vect(data)
-    dates <- lubridate::as_date(data$Date)
+    dates <- lubridate::as_datetime(data$Date)
   } else if (data_type == "Raster") {
     data <- terra::rast(data)
-    dates <- lubridate::as_date(data@ptr$time)
+    dates <- lubridate::as_datetime(data@ptr$time)
   }
   #### 3. check `dates` are within `range`
   checked <- data.table::data.table(dates %in% range)
