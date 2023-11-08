8ad5a5fe11a882d1260a350d2e55a10d629e42bc
mitchellmanware <memanware@gmail.com>
2023-11-08 14:05:58 -0500

    default start_range and end_range

--- tests/testthat/test-temporal_range.R
+++ tests/testthat/test-temporal_range.R
@@ -10,8 +10,6 @@ testthat::test_that("covariates are within temporal range.", {
   iswithin <- NULL
   for(p in seq_along(path)) {
     iswithin <- check_temporal_range(data = path[p],
-                                     start_range = "2018-01-01",
-                                     end_range = "2022-12-31",
                                      data_type = "Raster")
     iswithin <- rbind(iswithin, iswithin)
   }
@@ -24,8 +22,6 @@ testthat::test_that("test_nc_output.nc is within temporal range.", {
   # assumes that test_nc_output.nc is point data
   path <- "../testdata/test_nc_output.nc"
   iswithin <- check_temporal_range(data = path,
-                                   start_range = "2018-01-01",
-                                   end_range = "2022-12-31",
                                    data_type = "Point")
   # expect all elements equal true
   expect_equal(any(iswithin), TRUE)
