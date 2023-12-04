ab56cc9c159751f413bf5596b6a3d65430b29079
Eva Marques <marquesel@ehshpclp143.hpc.niehs.nih.gov>
2023-12-04 14:21:06 -0500

    #196 Refactoring nlcd ratio calculation scripts

--- R/calc_nlcd_ratio.R
+++ R/calc_nlcd_ratio.R
@@ -22,79 +22,71 @@ nlcd_classes <- list(value = c(0, 11, 21, 22, 23, 24, 31, 41, 42, 43, 52,
                                "Cultivated Crops",
                                "Woody Wetlands",
                                "Emergent Herbaceous Wetlands"),
-                     col = c("white", "#476ba1", "#decaca", "#d99482", "#ee0000",
-                             "#ab0000", "#b3aea3", "#68ab63", "#1c6330",
-                             "#b5ca8f", "#ccba7d",  "#e3e3c2", "#dcd93d", 
-                             "#ab7028", "#bad9eb", "#70a3ba")) 
+                     col = c("white", "#476ba1", "#decaca", "#d99482",
+                             "#ee0000", "#ab0000", "#b3aea3", "#68ab63",
+                             "#1c6330", "#b5ca8f", "#ccba7d",  "#e3e3c2",
+                             "#dcd93d", "#ab7028", "#bad9eb", "#70a3ba"))
 nlcd_classes <- as.data.frame(nlcd_classes)
 
 
 #' Compute land cover classes ratio in circle buffers around points
 #'
 #' @param data_vect terra::SpatVector of points geometry
-#' @param buf_radius numeric (non-negative) giving the radius of buffer around points 
+#' @param buf_radius numeric (non-negative) giving the
+#' radius of buffer around points
 #' @param year numeric giving the year of NLCD data used
 #' @export
-calc_nlcd_ratio <- function(data_vect, 
-                            buf_radius = 1000, 
+calc_nlcd_ratio <- function(data_vect,
+                            buf_radius = 1000,
                             year = 2021) {
-  
-  # check inputs 
-  if (!is.numeric(buf_radius)){
+  # check inputs
+  if (!is.numeric(buf_radius)) {
     stop("buf_radius is not a numeric.")
   }
   if (buf_radius <= 0) {
     stop("buf_radius has not a likely value.")
   }
-  if (!is.numeric(year)){
+  if (!is.numeric(year)) {
     stop("year is not a numeric.")
   }
   if (class(data_vect)[1] != "SpatVector") {
     stop("data_vect is not a terra::SpatVector")
   }
-  
   # open nlcd file corresponding to the year
   path_data <- "./input/nlcd/raw/"
-  nlcd_file <- list.files(path_data, 
-                          pattern = paste0("nlcd_", year, "_.*.tif$"), 
-                          full.names = TRUE) 
-  #nlcd_file <- list.files("/Volumes/set/NLCD/", 
-  #                        pattern = paste0("nlcd_", year, "_.*.tif$"), 
-  #                        full.names = TRUE) 
+  nlcd_file <- list.files(path_data,
+                          pattern = paste0("nlcd_", year, "_.*.tif$"),
+                          full.names = TRUE)
   if (length(nlcd_file) == 0) {
     stop("NLCD data not available for this year.")
   }
   nlcd <- rast(nlcd_file)
-  
   # select points within mainland US and reproject on nlcd crs if necessary
   # need spData library
   data(us_states)
   us_main <- st_union(us_states) %>%
     terra::vect() %>%
-    terra::project(x = ., y = crs(data_vect))
+    terra::project(y = crs(data_vect))
   data_vect_b <- data_vect %>%
-    terra::intersect(us_main, .)
+    terra::intersect(x = us_main)
   if (!same.crs(data_vect_b, nlcd)) {
     data_vect_b <- project(data_vect_b, crs(nlcd))
   }
-  
-  # create circle buffers with buf_radius 
+  # create circle buffers with buf_radius
   bufs_pol <- terra::buffer(data_vect_b, width = buf_radius) %>%
     sf::st_as_sf()
-  
   # ratio of each nlcd class per buffer
-  nlcd_at_bufs <- exact_extract(nlcd, 
-                                st_geometry(bufs_pol), 
+  nlcd_at_bufs <- exact_extract(nlcd,
+                                st_geometry(bufs_pol),
                                 fun = "frac",
-                                stack_apply = T, 
-                                progress = F)
-  
+                                stack_apply = TRUE,
+                                progress = FALSE)
   # select only the columns of interest
   nlcd_at_bufs <- nlcd_at_bufs[names(nlcd_at_bufs)[grepl("frac_",
-                                                       names(nlcd_at_bufs))]]
+                                                         names(nlcd_at_bufs))]]
   # change column names
   nlcd_names <- names(nlcd_at_bufs) %>%
-    sub("frac_", "", .) %>%
+    sub(pattern = "frac_", replacement = "") %>%
     as.numeric()
   nlcd_names <- nlcd_classes[nlcd_classes$value %in% nlcd_names, c("class")]
   new_names <- sapply(
@@ -104,12 +96,8 @@ calc_nlcd_ratio <- function(data_vect,
     }
   )
   names(nlcd_at_bufs) <- new_names
-  
   # merge data_vect with nlcd class fractions (and reproject)
-  new_data_vect <- cbind(data_vect_b, nlcd_at_bufs) %>% 
-      project(., crs(data_vect)) 
-  
+  new_data_vect <- cbind(data_vect_b, nlcd_at_bufs) %>%
+    project(crs(data_vect))
   return(new_data_vect)
 }
-
-
