4459203de4d2b6d98828abd0dadb3bcbfffe06b6
Eva Marques <marquesel@ehshpclp143.hpc.niehs.nih.gov>
2023-12-05 14:02:08 -0500

    Corrections from Insang review

--- R/calc_nlcd_ratio.R
+++ R/calc_nlcd_ratio.R
@@ -66,7 +66,7 @@ calc_nlcd_ratio <- function(data_vect,
   nlcd_at_bufs <- nlcd_at_bufs[names(nlcd_at_bufs)[grepl("frac_",
                                                          names(nlcd_at_bufs))]]
   # change column names
-  fpath <- system.file("extdata", "nlcd_classes.csv", package="NRTAPmodel")
+  fpath <- system.file("extdata", "nlcd_classes.csv", package = "NRTAPmodel")
   nlcd_classes <- read.csv(fpath)
   nlcd_names <- names(nlcd_at_bufs) %>%
     sub(pattern = "frac_", replacement = "") %>%
