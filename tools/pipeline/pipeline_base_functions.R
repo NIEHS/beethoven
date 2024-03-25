b3853ff514bb94cad012b4fc0a40699d0856eda5
Insang Song <insang.song@nih.gov>
2024-03-25 15:22:00 -0400

    transfer bulk geos processing function to pipeline base

--- tools/pipeline/pipeline_base_functions.R
+++ tools/pipeline/pipeline_base_functions.R
@@ -446,3 +446,432 @@ df_params <- function(functions) {
 # schec <- search_params("amadeus", "calc_")
 # df_params(sched[-c(1, 2, 3, 4, 5, 6, 8, 11, 14, 15, 17, 18, 19, 20, 21, 25)])
 # df_params(schec[-c(1, 16)]) |> colnames()
+
+
+
+
+#' Process atmospheric composition data by chunks
+#' @description
+#' Returning a single `SpatRaster` object.
+#' @param date character(2). length of 10. Format "YYYY-MM-DD".
+#' @param path character(1). Directory with downloaded netCDF (.nc4) files. or
+#' netCDF file paths.
+#' @param ... Arguments passed to [`terra::rast`].
+#' @note
+#' Layer names of the returned `SpatRaster` object contain the variable,
+#' pressure level, date.
+#' @author Mitchell Manware
+#' @return a `SpatRaster` object;
+#' @importFrom terra rast
+#' @importFrom terra time
+#' @importFrom terra varnames
+#' @importFrom terra crs
+#' @importFrom terra subset
+#' @export
+process_geos_bulk_old <-
+  function(path = NULL,
+           date = c("2018-01-01", "2018-01-01"),
+           ...) {
+    #### directory setup
+    if (length(path) == 1) {
+
+      if (dir.exists(path)) {
+      path <- amadeus::download_sanitize_path(path)
+      paths <- list.files(
+        path,
+        pattern = "GEOS-CF.v01.rpl",
+        full.names = TRUE
+      )
+      paths <- paths[grep(
+        ".nc4",
+        paths
+      )]
+    }
+    } else {
+      paths <- path
+    }
+    #### check for variable
+    amadeus::check_for_null_parameters(mget(ls()))
+    #### identify file paths
+    #### identify dates based on user input
+    dates_of_interest <- amadeus::generate_date_sequence(
+      date[1],
+      date[2],
+      sub_hyphen = TRUE
+    )
+    #### subset file paths to only dates of interest
+    data_paths <- unique(
+      grep(
+        paste(
+          dates_of_interest,
+          collapse = "|"
+        ),
+        paths,
+        value = TRUE
+      )
+    )
+    #### identify collection
+    collection <- amadeus::process_collection(
+      data_paths[1],
+      source = "geos",
+      collection = TRUE
+    )
+    cat(
+      paste0(
+        "Identified collection ",
+        collection,
+        ".\n"
+      )
+    )
+    if (length(unique(collection)) > 1) {
+      warning(
+        "Multiple collections detected. Returning data for all collections.\n"
+      )
+    }
+
+    filename_date <- regmatches(
+      data_paths,
+      regexpr(
+        "20[0-9]{2}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])",
+        data_paths
+      )
+    )
+    if (any(table(filename_date) < 24)) {
+      warning(
+        "Some dates include less than 24 hours. Check the downloaded files."
+      )
+    }
+    if (length(unique(filename_date)) > 10) {
+      message("More than 10 unique dates detected. Try 10-day chunks...")
+    }
+
+    # split filename date every 10 days
+    filename_date <- as.Date(filename_date, format = "%Y%m%d")
+    filename_date_cl <- as.integer(cut(filename_date, "30 days"))
+
+    future_inserted <- split(data_paths, filename_date_cl)
+    other_args <- list(...)
+    data_variables <- names(terra::rast(data_paths[1]))
+
+    summary_byvar <- function(x = data_variables, fs) {
+      rast_in <- rlang::inject(terra::rast(fs, !!!other_args))
+      terra::sds(lapply(
+        x,
+        function(v) {
+          rast_inidx <- grep(v, names(rast_in))
+          rast_in <- rast_in[[rast_inidx]]
+          rast_summary <- terra::tapp(rast_in, index = "days", fun = "mean")
+          names(rast_summary) <-
+            paste0(
+              rep(v, terra::nlyr(rast_summary)), "_", terra::time(rast_summary)
+            )
+          terra::set.crs(rast_summary, "EPSG:4326")
+          return(rast_summary)
+        }
+      ))
+    }
+
+    # summary by 10 days
+    # TODO: dropping furrr?
+    rast_10d_summary <-
+      furrr::future_map(
+        .x = future_inserted,
+        .f = ~summary_byvar(fs = .x),
+        .options = furrr::furrr_options(
+          globals = c("other_args", "data_variables")
+        )
+      )
+    rast_10d_summary <- Reduce(c, rast_10d_summary)
+    return(rast_10d_summary)
+
+  }
+
+
+
+#' Process atmospheric composition data by chunks (v2)
+#' @description
+#' Returning a single `SpatRasterDataset` object.
+#' @param date character(2). length of 10. Format "YYYY-MM-DD".
+#' @param path character(1). Directory with downloaded netCDF (.nc4) files. or
+#' netCDF file paths.
+#' @param ... Arguments passed to [`terra::rast`].
+#' @note
+#' Layer names of the returned `SpatRaster` object contain the variable,
+#' pressure level, date
+#' Reference duration: 1 day summary, all layers: 115 seconds
+#' @author Mitchell Manware, Insang Song
+#' @return a `SpatRaster` object;
+#' @importFrom terra rast
+#' @importFrom terra time
+#' @importFrom terra varnames
+#' @importFrom terra crs
+#' @importFrom terra subset
+#' @export
+process_geos_bulk <-
+  function(path = NULL,
+           date = c("2018-01-01", "2018-01-01"),
+           ...) {
+    #### directory setup
+    if (length(path) == 1) {
+
+      if (dir.exists(path)) {
+      path <- amadeus::download_sanitize_path(path)
+      paths <- list.files(
+        path,
+        pattern = "GEOS-CF.v01.rpl",
+        full.names = TRUE
+      )
+      paths <- paths[grep(
+        ".nc4",
+        paths
+      )]
+    }
+    } else {
+      paths <- path
+    }
+    #### check for variable
+    amadeus::check_for_null_parameters(mget(ls()))
+    #### identify file paths
+    #### identify dates based on user input
+    dates_of_interest <- amadeus::generate_date_sequence(
+      date[1],
+      date[2],
+      sub_hyphen = TRUE
+    )
+    #### subset file paths to only dates of interest
+    data_paths <- unique(
+      grep(
+        paste(
+          dates_of_interest,
+          collapse = "|"
+        ),
+        paths,
+        value = TRUE
+      )
+    )
+    #### identify collection
+    collection <- amadeus::process_collection(
+      data_paths[1],
+      source = "geos",
+      collection = TRUE
+    )
+    cat(
+      paste0(
+        "Identified collection ",
+        collection,
+        ".\n"
+      )
+    )
+    if (length(unique(collection)) > 1) {
+      warning(
+        "Multiple collections detected. Returning data for all collections.\n"
+      )
+    }
+
+    filename_date <- regmatches(
+      data_paths,
+      regexpr(
+        "20[0-9]{2}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])",
+        data_paths
+      )
+    )
+    if (any(table(filename_date) < 24)) {
+      warning(
+        "Some dates include less than 24 hours. Check the downloaded files."
+      )
+    }
+    if (length(unique(filename_date)) > 10) {
+      message(
+        "More than 10 unique dates detected. Try 10-day chunks..."
+      )
+    }
+
+    # split filename date every 10 days
+    filename_date <- as.Date(filename_date, format = "%Y%m%d")
+    filename_date_cl <- as.integer(cut(filename_date, "30 days"))
+
+    future_inserted <- split(data_paths, filename_date_cl)
+    other_args <- list(...)
+    data_variables <- names(terra::rast(data_paths[1]))
+
+    summary_byvar <- function(x = data_variables, fs) {
+      #do.call(c, 
+      rast_in <- rlang::inject(terra::rast(fs, !!!other_args))
+      terra::sds(lapply(
+        x,
+        function(v) {
+          rast_inidx <- grep(v, names(rast_in))
+          rast_in <- rast_in[[rast_inidx]]
+          rast_summary <- terra::tapp(rast_in, index = "days", fun = "mean")
+          names(rast_summary) <-
+            paste0(
+              rep(v, terra::nlyr(rast_summary)), "_",
+              terra::time(rast_summary)
+            )
+          terra::set.crs(rast_summary, "EPSG:4326")
+          return(rast_summary)
+        }
+      ))
+    }
+
+    # summary by 10 days
+    # TODO: dropping furrr?
+    rast_10d_summary <-
+      furrr::future_map(
+        .x = future_inserted,
+        .f = ~summary_byvar(fs = .x),
+        .options =
+        furrr::furrr_options(
+          globals = c("other_args", "data_variables")
+        )
+      )
+    rast_10d_summary <- Reduce(c, rast_10d_summary)
+    return(rast_10d_summary)
+
+  }
+
+#' Process atmospheric composition data by chunks (v3)
+#' @description
+#' Returning a single `SpatRasterDataset` object.
+#' Removed `tapp` for performance
+#' @param date character(2). length of 10. Format "YYYY-MM-DD".
+#' @param path character(1). Directory with downloaded netCDF (.nc4) files. or
+#' netCDF file paths.
+#' @param ... Arguments passed to [`terra::rast`].
+#' @note
+#' Layer names of the returned `SpatRaster` object contain the variable,
+#' pressure level, date
+#' Reference duration: 1 day summary, all layers: 106 seconds
+#' @author Mitchell Manware, Insang Song
+#' @return a `SpatRaster` object;
+#' @importFrom terra rast
+#' @importFrom terra time
+#' @importFrom terra varnames
+#' @importFrom terra crs
+#' @importFrom terra subset
+#' @export
+process_geos_strict <-
+  function(path = NULL,
+           date = c("2018-01-01", "2018-01-01"),
+           ...) {
+    #### directory setup
+    if (length(path) == 1) {
+
+      if (dir.exists(path)) {
+        path <- amadeus::download_sanitize_path(path)
+        paths <- list.files(
+          path,
+          pattern = "GEOS-CF.v01.rpl",
+          full.names = TRUE
+        )
+        paths <- paths[grep(
+          ".nc4",
+          paths
+        )]
+      }
+    } else {
+      paths <- path
+    }
+    #### check for variable
+    amadeus::check_for_null_parameters(mget(ls()))
+    #### identify file paths
+    #### identify dates based on user input
+    dates_of_interest <- amadeus::generate_date_sequence(
+      date[1],
+      date[2],
+      sub_hyphen = TRUE
+    )
+    #### subset file paths to only dates of interest
+    data_paths <- unique(
+      grep(
+        paste(
+          dates_of_interest,
+          collapse = "|"
+        ),
+        paths,
+        value = TRUE
+      )
+    )
+    #### identify collection
+    collection <- amadeus::process_collection(
+      data_paths[1],
+      source = "geos",
+      collection = TRUE
+    )
+    cat(
+      paste0(
+        "Identified collection ",
+        collection,
+        ".\n"
+      )
+    )
+    if (length(unique(collection)) > 1) {
+      warning(
+        "Multiple collections detected. Returning data for all collections.\n"
+      )
+    }
+
+    filename_date <- regmatches(
+      data_paths,
+      regexpr(
+        "20[0-9]{2}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])",
+        data_paths
+      )
+    )
+    if (any(table(filename_date) < 24)) {
+      warning(
+        "Some dates include less than 24 hours. Check the downloaded files."
+      )
+    }
+    if (length(unique(filename_date)) > 10) {
+      message("More than 10 unique dates detected. Try 10-day chunks...")
+    }
+
+    # split filename date every 10 days
+    filename_date <- as.Date(filename_date, format = "%Y%m%d")
+    filename_date_cl <- as.integer(cut(filename_date, "10 days"))
+
+    future_inserted <- split(data_paths, filename_date_cl)
+    other_args <- list(...)
+    data_variables <- names(terra::rast(data_paths[1]))
+
+    summary_byvar <- function(x = data_variables, fs) {
+      rast_in <- rlang::inject(terra::rast(fs, !!!other_args))
+      terra::sds(lapply(
+        x,
+        function(v) {
+          rast_inidx <- grep(v, names(rast_in))
+          rast_in <- rast_in[[rast_inidx]]
+          rtin <- as.Date(terra::time(rast_in))
+          rtin_u <- unique(rtin)
+          rast_summary <- vector("list", length = length(unique(rtin)))
+          for (d in seq_along(rtin_u)) {
+            rast_d <- rast_in[[rtin == rtin_u[d]]]
+            rast_summary[[d]] <- mean(rast_d)
+          }
+          rast_summary <- do.call(c, rast_summary)
+          # rast_summary <- terra::tapp(rast_in, index = "days", fun = "mean")
+          names(rast_summary) <-
+            paste0(
+              rep(v, terra::nlyr(rast_summary)), "_", terra::time(rast_summary)
+            )
+          terra::set.crs(rast_summary, "EPSG:4326")
+          return(rast_summary)
+        }
+      ))
+    }
+
+    # summary by 10 days
+    # TODO: dropping furrr?
+    rast_10d_summary <-
+      furrr::future_map(
+        .x = future_inserted,
+        .f = ~summary_byvar(fs = .x),
+        .options = furrr::furrr_options(
+          globals = c("other_args", "data_variables")
+        )
+      )
+    rast_10d_summary <- do.call(c, rast_10d_summary)
+    return(rast_10d_summary)
+
+  }
