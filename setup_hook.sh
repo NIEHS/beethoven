8a33177f1bf8d31455ecc656546d35cd16e26104
Insang Song <insang.song@nih.gov>
2024-07-15 16:15:29 -0400

    another try

--- setup_hook.sh
+++ setup_hook.sh
@@ -2,5 +2,5 @@
 
 HOOKS_DIR=$(git rev-parse --show-toplevel)/.git/hooks
 echo '#!/bin/bash' > $HOOKS_DIR/post-checkout
-echo 'chmod 554 _targets.R' >> $HOOKS_DIR/post-checkout
+echo 'chmod 740 _targets.R' >> $HOOKS_DIR/post-checkout
 chmod +x $HOOKS_DIR/post-checkout
