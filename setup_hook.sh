68ed2a698f4983ecb49bc8101b0a9fd07752c6b2
Insang Song <insang.song@nih.gov>
2024-07-15 16:49:47 -0400

    hook update

--- setup_hook.sh
+++ setup_hook.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 HOOKS_DIR=$(git rev-parse --show-toplevel)/.git/hooks
-echo '#!/bin/bash' > $HOOKS_DIR/post-checkout
-echo 'chmod 740 _targets.R' >> $HOOKS_DIR/post-checkout
+cat > $HOOKS_DIR/post-checkout << EOF
+chmod 740 _targets.R
+#!/bin/bash
+# Define the users who should have write privileges
+WRITE_USERS=("messierkp")
+ 
+# Get the current username
+CURRENT_USER=\$(whoami)
+ 
+# Set default permissions (read and execute)
+DEFAULT_PERMISSIONS=554
+ 
+# Set write permissions (read, write, and execute)
+WRITE_PERMISSIONS=754
+ 
+# Function to set permissions
+set_permissions() {
+    local permissions=\$1
+    local file=\$2
+    chmod \$permissions \$file
+}
+ 
+# Check if the current user is in the list of write users
+if [[ " \${WRITE_USERS[@]} " =~ " \${CURRENT_USER} " ]]; then
+    set_permissions \$WRITE_PERMISSIONS LICENSE
+else
+    set_permissions \$DEFAULT_PERMISSIONS LICENSE
+fi
+EOF
+
 chmod +x $HOOKS_DIR/post-checkout
+
+ 
