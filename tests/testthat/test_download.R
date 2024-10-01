30b0f89421c6c4d39738b0caa8b0cb0112ac9bf2
kyle-messier <kyle.messier@nih.gov>
2024-10-01 13:32:11 -0400

    def files, bash scipts for containers, testing yaml

---
+++ tests/testthat/test_download.R
@@ -0,0 +1,13 @@
+################################################################################
+##### unit and integration tests for data download
+##### Currently arbitrary placeholders
+##### 
+
+################################################################################
+##### loadargs
+testthat::test_that("download targets work", {
+
+ a <- 1
+ testthat::expect_equal(a, 1)
+
+})
