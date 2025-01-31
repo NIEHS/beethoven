684b50dff9d7a9cf9e99dd4a955f56bb0b4537f4
mitchellmanware <memanware@gmail.com>
2025-01-31 11:10:22 -0500

    glue -> paste; cov + test keywords

--- R/helpers.R
+++ R/helpers.R
@@ -40,22 +40,24 @@ gpu <- function() {
 #' Run all tests within a single file from `tests/testthat/` directory
 #' with the `container_models.sif` container.
 #' @param pattern A regular expression to match the test file name.
-#' @return NULL; Prints the output of the testthat tests.
-#' @importFrom glue glue
+#' @return NULL; Prints the output of the `testthat` tests.
 #' @seealso [testthat::test_file()]
-#' @importFrom glue glue
+#' @keywords Miscellaneous
 test <- function(pattern = NULL) {
   if (is.null(pattern)) stop()
   system(
-    glue::glue(
-      "apptainer exec --nv --bind $PWD:/mnt --bind /tmp:/opt/tmp ",
-      "container_models.sif Rscript --no-init-file -e \"",
-      ".libPaths(grep(paste0('biotools|', Sys.getenv('USER')), .libPaths(), ",
-      "value = TRUE, invert = TRUE)); devtools::load_all('/mnt'); ",
-      "library(bonsai); library(dplyr); library(testthat); ",
-      "test_file <- list.files('/mnt/tests/testthat', full.names = TRUE, ",
-      "pattern = '", pattern, "'); source_files <- list.files('/mnt/R', ",
-      "full.names = TRUE); covr::file_coverage(source_files, test_file)\""
+    paste(
+      c(
+        "apptainer exec --nv --bind $PWD:/mnt --bind /tmp:/opt/tmp ",
+        "container_models.sif Rscript --no-init-file -e \"",
+        ".libPaths(grep(paste0('biotools|', Sys.getenv('USER')), .libPaths(), ",
+        "value = TRUE, invert = TRUE)); devtools::load_all('/mnt'); ",
+        "library(bonsai); library(dplyr); library(testthat); ",
+        "test_file <- list.files('/mnt/tests/testthat', full.names = TRUE, ",
+        "pattern = '", pattern, "'); source_files <- list.files('/mnt/R', ",
+        "full.names = TRUE); covr::file_coverage(source_files, test_file)\""
+      ),
+      collapse = ""
     )
   )
 }
@@ -64,18 +66,21 @@ test <- function(pattern = NULL) {
 #' `container_models.sif` container.
 #' @return NULL; Prints the output of the code coverage.
 #' @seealso [covr::package_coverage()]; [covr::coverage_to_list()]
-#' @importFrom glue glue
+#' @keywords Miscellaneous
 cov <- function() {
   system(
-    glue::glue(
-      "apptainer exec --nv --bind $PWD:/mnt --bind /tmp:/opt/tmp ",
-      "container_models.sif Rscript --no-init-file -e \"",
-      ".libPaths(grep(paste0('biotools|', Sys.getenv('USER')), .libPaths(), ",
-      "value = TRUE, invert = TRUE)); devtools::load_all('/mnt'); ",
-      "library(bonsai); library(dplyr); library(testthat); ",
-      "cov <- covr::package_coverage(install_path = '/tmp/cov', ",
-      "clean = FALSE); ",
-      "saveRDS(cov, '/mnt/cov_0131.rds'); covr::coverage_to_list(cov)\""
+    paste(
+      c(
+        "apptainer exec --nv --bind $PWD:/mnt --bind /tmp:/opt/tmp ",
+        "container_models.sif Rscript --no-init-file -e \"",
+        ".libPaths(grep(paste0('biotools|', Sys.getenv('USER')), .libPaths(), ",
+        "value = TRUE, invert = TRUE)); devtools::load_all('/mnt'); ",
+        "library(bonsai); library(dplyr); library(testthat); ",
+        "cov <- covr::package_coverage(install_path = '/tmp/cov', ",
+        "clean = FALSE); ",
+        "saveRDS(cov, '/mnt/cov_0131.rds'); covr::coverage_to_list(cov)\""
+      ),
+      collapse = ""
     )
   )
 }
